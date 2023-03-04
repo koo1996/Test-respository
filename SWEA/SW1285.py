7e7cfe577b240e7d2f57eba8463f45389b58c8b8
koo1996 <koodo1996@gmail.com>
2023-03-04 16:38:32 +0900

    update

--- SWEA/SW1285.py
+++ SWEA/SW1285.py
@@ -1,3 +1,5 @@
+#1번 풀이
+
 T = int(input())
 
 for tc in range(1, T+1):
@@ -10,3 +12,18 @@ for tc in range(1, T+1):
     small = min(list_)
     count_ = list_.count(small)
     print('#{} {} {}'.format(tc, small, count_))
+
+# 2번 풀이
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    N_list = list(map(int,input().split()))
+    length = abs(N_list[0])
+    result = 0
+    for i in N_list:
+        if length > abs(i):
+            length = abs(i)        
+    result += N_list.count(length)
+    result += N_list.count(-1*length)
+    print('#{} {} {}'.format(tc, length, result))
