dee86df7dc13a3acafe04a21d522677d5cf2d995
koo1996 <koodo1996@gmail.com>
2023-03-18 09:47:15 +0900

    update

--- SWEA/SW14555.py
+++ SWEA/SW14555.py
@@ -3,19 +3,7 @@ T = int(input())
 for tc in range(1,T+1):
     qu = input()
     cnt = 0
-    for i in range(len(qu)):
-        if qu[i] == '(':
-            if qu[i+1] == '|':
-                cnt += 1
-            elif qu[i+1] == ')':
-                cnt += 1
-            else:
-                break
-        elif qu[i] == '|':
-            if qu[i+1] == ')':
-                cnt += 1
-            else:
-                break
-        else:
-            break
+    for i in range(len(qu)-1):
+        if qu[i]+qu[i+1] == '(|' or qu[i]+qu[i+1] == '|)' or qu[i]+qu[i+1] == '()':
+            cnt += 1
     print(f'#{tc} {cnt}')
