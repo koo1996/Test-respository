9e2414d83fa4b2d7c87448ac067fc119615416e8
koo1996 <koodo1996@gmail.com>
2023-03-19 18:08:00 +0900

    update

---
+++ SWEA/SW13547.py
@@ -0,0 +1,9 @@
+T = int(input())
+
+for tc in range(1,T+1): # 테스트케이스
+    qu = input() 
+    if qu.count('x') >= 8: # 게임을 15판중에 8판 이상 졌을때 이길 가능성이 없다고 봅니다 answer = 'NO'
+        answer = 'NO'
+    else:
+        answer = 'YES'
+    print(f'#{tc} {answer}')
