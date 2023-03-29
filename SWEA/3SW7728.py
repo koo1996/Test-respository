2fb2be8730110d189d27175bcdc76cdb025cb0ee
koo1996 <koodo1996@gmail.com>
2023-03-29 16:57:39 +0900

    upade

---
+++ SWEA/3SW7728.py
@@ -0,0 +1,7 @@
+T = int(input())
+
+dic_ = {'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
+for  tc in range(1,T+1):
+    qu = input()
+    for i in range(len(qu)):
+        dic_[qu[i]]
