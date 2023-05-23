c3683fc3d2e877cac5e42c35c221b6903a71c698
koo1996 <koodo1996@gmail.com>
2023-05-23 16:09:07 +0900

    update

---
+++ Java/ch03_type.java
@@ -0,0 +1,8 @@
+public class ch03_type {
+    public static void main(String[] args) {
+        int i = Integer.parseInt("93");
+        System.out.println(i);
+        double d = Double.parseDouble("98.8");
+        System.out.println(d);
+    }
+}
