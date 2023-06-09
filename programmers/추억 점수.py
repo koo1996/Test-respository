c05ed5d80c5d241da1e97c2a3235089946d9754f
koo1996 <koodo1996@gmail.com>
2023-06-09 11:44:06 +0900

    update

---
+++ programmers/추억 점수.py
@@ -0,0 +1,11 @@
+def solution(name, yearning, photo):
+    answer = []
+    info = dict(zip(name, yearning))
+    
+    for i in photo:
+        score = 0
+        for j in i:
+            score += info.get(j, 0)
+        answer.append(score)
+    
+    return answer
