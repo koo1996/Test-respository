d5c088d166ba90b69aa8162629adc8821f757e2a
koo1996 <koodo1996@gmail.com>
2023-06-10 15:32:50 +0900

    update

---
+++ programmers/문자열 겹쳐쓰기.py
@@ -0,0 +1,7 @@
+def solution(my_string, overwrite_string, s):
+    answer = ''
+
+    if len(overwrite_string) - (len(my_string) - s) >= 0:
+        return my_string[:s] + overwrite_string
+    else:
+        return my_string[:s] + overwrite_string + my_string[s+len(overwrite_string):]
