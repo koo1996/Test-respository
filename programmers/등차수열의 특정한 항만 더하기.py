98f5955597c9337c3003ebeae57a79291148cd45
koo1996 <koodo1996@gmail.com>
2023-06-16 17:12:19 +0900

    update

--- programmers/등차수열의 특정한 항만 더하기.py
+++ programmers/등차수열의 특정한 항만 더하기.py
@@ -1,9 +1,7 @@
-a = 3
-d = 4
-included = [true, false, false, true, true]
-answer = 0
-for i in range(len(included)):
-    a += i * d
-    if included[i] == "true":
-        answer += a
-print(answer)
+def solution(a, d, included):
+    answer = 0
+    for i in range(len(included)):
+        if included[i]:
+            answer += a    
+        a += d
+    return answer
