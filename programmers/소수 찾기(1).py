17cd44ad415e321ec4a50ccbc6d56be9972eb1d3
koo1996 <koodo1996@gmail.com>
2023-07-02 00:41:10 +0900

    update

--- programmers/소수 찾기(1).py
+++ programmers/소수 찾기(1).py
@@ -1,10 +1,9 @@
-n = 10
-answer = 0    
-for i in range(2,n+1):
-    cnt = 0
-    for j in range(1,i+1):
-        if i % j != 0:
-            break
-    answer += 1
-    print(i)
-print(answer)
+def solution(n):
+    answer = 1    
+    for i in range(3,n+1):
+        for j in range(2,int(i ** (1/2))+1):
+            if i % j == 0:
+                break
+        else:
+            answer += 1
+    return answer
