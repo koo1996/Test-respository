da6d659818992798aa35fda0d7896ce16349cef9
koo1996 <koodo1996@gmail.com>
2023-07-11 01:28:09 +0900

    update

---
+++ programmers/연속된 부분 수열의 합(2).py
@@ -0,0 +1,17 @@
+sequence = [1, 2, 3, 4, 5]
+k = 2
+
+answer = []
+right = 0
+cnt = 0
+for left in range(len(sequence)):
+    
+    while right < len(sequence)
+    if sequence[left] == k:
+
+    # if sequence[i] == k:
+    #     answer[0] = i
+    #     answer[1] = i
+    # else:
+    #     sequence[i+1] 
+print(answer)
