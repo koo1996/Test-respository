07c8eb2f9a81767e0ba5b60121a999e33cdd4eca
koo1996 <koodo1996@gmail.com>
2023-07-12 16:18:18 +0900

    update

--- programmers/연속된 부분 수열의 합(2).py
+++ programmers/연속된 부분 수열의 합(2).py
@@ -1,17 +1,20 @@
-sequence = [1, 2, 3, 4, 5]
-k = 2
-
-answer = []
-right = 0
-cnt = 0
-for left in range(len(sequence)):
-    
-    while right < len(sequence)
-    if sequence[left] == k:
-
-    # if sequence[i] == k:
-    #     answer[0] = i
-    #     answer[1] = i
-    # else:
-    #     sequence[i+1] 
-print(answer)
+def solution(sequence, k):
+    answer = []
+    right = 0
+    cnt = 0
+    for left in range(len(sequence)):    
+        while right < len(sequence) and cnt < k:
+            cnt += sequence[right]
+            right += 1
+            
+        if cnt == k:
+            if not answer:
+                answer = [left, right-1]
+            else:
+                if answer[1] - answer[0] > right - 1 - left:
+                    answer = [left, right-1]
+                else:
+                    answer
+        
+        cnt -= sequence[left]  
+    return answer
