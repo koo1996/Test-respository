f0770bee7a138434215a23976af259cb7de69628
koo1996 <koodo1996@gmail.com>
2023-09-04 00:32:25 +0900

    update

---
+++ Java/Study/day24.java
@@ -0,0 +1,26 @@
+public class day24 {
+    public static int getRandom(int n){
+        return (int)(Math.random()*n)+1;
+    }
+    public static int getRandom(int n1,int n2){
+        return (int)(Math.random()*n1+(n2-n1))+1;
+    }
+    public static void main(String[] args){
+        for (int i=0; i<5; i++){                        
+            if (i==4){
+                System.out.print(getRandom(10)+"");
+            } else{
+                System.out.print(getRandom(10)+", ");
+            }
+        }
+        System.out.println();
+        for (int i=0; i<5; i++){                        
+            if (i==4){
+                System.out.print(getRandom(10,20)+"");
+            } else{
+                System.out.print(getRandom(10,20)+", ");
+            }
+        }
+    }   
+
+}
